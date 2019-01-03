df84eb6eb31a2fc1b84534ff3e9a98047c055d95
Khaled Hossain Ansary <ansary.development@gmail.com>
2019-01-03 18:55:37 +0100

    configure jasemine

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,7 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        
 
 
         /* TODO: Write a test that loops through each feed
